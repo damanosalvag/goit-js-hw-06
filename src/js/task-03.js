b569dbfb13fcb666421d38d474d056e241a2534e
Dairo Manosalva <38769711+damanosalvag@users.noreply.github.com>
2023-03-24 15:25:20 -0500

    ready task 3

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -13,18 +13,9 @@ const images = [
   },
 ];
 
-
-
-
-const categoriesList = document.querySelector('#categories');
-const categoriesItems = categoriesList.querySelectorAll('.item');
-
-console.log(`Number of categories: ${categoriesItems.length}`);
-
-categoriesItems.forEach(item => {
-    const categoryTitle = item.querySelector('h2').textContent;
-    const categoryElements = item.querySelectorAll('li').length;
-
-    console.log(`Category: ${categoryTitle}`);
-    console.log(`Elements: ${categoryElements}`);
-});
+const ul = document.querySelector(".gallery");
+const liElementGallery = images.map(image => {
+  const li = `<li><img src='${image.url}' alt='${image.alt}'></img></li>`;
+  return li;
+}).join(' ');
+ul.insertAdjacentHTML('beforeend', liElementGallery);
